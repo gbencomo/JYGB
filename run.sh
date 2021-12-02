fd2cfa6044edc62a5f6ff1c17fc200e81cda0635
Gianluca <gb5435@princeton.edu>
2021-12-01 23:29:09 -0500

    Made some temp changes to shell script

--- run.sh
+++ run.sh
@@ -6,11 +6,16 @@ echo "Starting pipeline..."
 set -euo pipefail
 IFS=$'\n\t'
 
+# Specify dataset, first argument
 DATASET=${1:-1}
 
+# Directory to save outputs for each run
 OUTPUT_DIR=results/${DATASET}
 RNG_SEED=$RANDOM
 OUTPUT_DIR+="_${RNG_SEED}"
 
 mkdir -p ${OUTPUT_DIR}
 
+# copy both standard output and standard error streams to file while still 
+# being visible in the terminal.  Append if file already exists
+python scratch.py 2>&1 | tee -a ${OUTPUT_DIR}/log.txt
