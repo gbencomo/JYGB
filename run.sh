c8df080aa545c9e2c1b9fc16418935faa2be5f52
Gianluca <gb5435@princeton.edu>
2021-12-12 23:25:29 -0500

    Fixed bash shell script

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 # "unofficial bash strict mode"
 set -euo pipefail
 IFS=$'\n\t'
+set -e
 
 # Specify dataset, first argument
 DATASET=${1:-1}
@@ -11,30 +12,28 @@ OUTPUT_DIR=results/${DATASET}
 RNG_SEED=$RANDOM
 OUTPUT_DIR+="_${RNG_SEED}"
 
-LEARNING_RATE=1e-2
-EPOCHS=200
+LEARNING_RATE=0.001
+EPOCHS=300
 BATCH_SIZE=16
 [ "$DATASET" == "mbm" ] && BATCH_SIZE=15
-AUGMENT=1
 FILTERS=64
 CONV=2
-WEIGHT_DECAY=1e-3
+WEIGHT_DECAY=0.001
 MOMENTUM=0.9
-VAL_PERCENT=0.2
+
+mkdir -p ${OUTPUT_DIR}
 
 # train model with above conditions; save output
 python train.py \
 -d ${DATASET} \
+-sp ${OUTPUT_DIR} \
 -lr ${LEARNING_RATE} \
 -e ${EPOCHS} \
 -b ${BATCH_SIZE} \
--a ${AUGMENT} \
 -uf ${FILTERS} \
 -c ${CONV} \
 -wd ${WEIGHT_DECAY} \
 -m ${MOMENTUM} \
 -s ${RNG_SEED} \
--v ${VAL_PERCENT} \
--sp ${OUTPUT_DIR} \ 
 --plot \
-2>&1 | tee -a ${OUTPUT_DIR}/log.txt
+#2>&1 | tee -a ${OUTPUT_DIR}/log.txt
